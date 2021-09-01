09fce9c7a79d7c0e2cfdbb72ab5ab2bd8256042a
sainanduru <saiprakash.nanduru@gmail.com>
2021-08-31 22:33:21 -0400

    Complete the happy path test using mockWebServer

--- jokes/src/test/java/com/learning/fun/jokes/dao/ChuckNorrisDaoTest.java
+++ jokes/src/test/java/com/learning/fun/jokes/dao/ChuckNorrisDaoTest.java
@@ -1,11 +1,16 @@
 package com.learning.fun.jokes.dao;
 
-import com.squareup.okhttp.HttpUrl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.learning.fun.jokes.model.response.Joke;
 import com.squareup.okhttp.mockwebserver.MockResponse;
 import com.squareup.okhttp.mockwebserver.MockWebServer;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -13,32 +18,41 @@ import java.io.IOException;
 
 class ChuckNorrisDaoTest {
 
-    private MockWebServer mockWebServer;
+    private static MockWebServer mockWebServer;
+    private static String chuckNorrisHost;
+    private ObjectMapper objectMapper = new ObjectMapper();
 
-    @Value("${chucknorris.api.host}")
-    String apiHost;
+    @BeforeAll
+    static void setup() throws IOException {
 
-    @BeforeEach
-    void setupMockWebServer(){
-        this.mockWebServer = new MockWebServer();
+        // Instantiate and start mock server
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+
+        // override the targetHost
+        chuckNorrisHost = "http://localhost:" + String.valueOf(mockWebServer.getPort());
     }
 
     @Test
-    void testForValidIntegration() throws IOException, InterruptedException {
-        mockWebServer.enqueue(new MockResponse().setBody("test joke"));
-        mockWebServer.start(8080);
-        HttpUrl baseUrl = mockWebServer.url("/jokes/random?category=test");
+    void testForValidIntegration() throws JsonProcessingException {
 
+        // sets mockServer response
+        Joke testJoke = new Joke();
+        testJoke.setValue("test joke");
+        mockWebServer.enqueue(new MockResponse().setBody(objectMapper.writeValueAsString(testJoke)).addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE));
 
+        //calls the actual class by setting private field values
         ChuckNorrisDao chuckNorrisDao = new ChuckNorrisDao(WebClient.builder());
-        ReflectionTestUtils.setField(chuckNorrisDao,"apiHost", "http://localhost");
+        ReflectionTestUtils.setField(chuckNorrisDao,"apiHost", chuckNorrisHost);
 
-//        System.out.println(mockWebServer.takeRequest());
+        //validations
+        String actualJoke = chuckNorrisDao.getJoke("test");
+        Assertions.assertEquals(testJoke.getValue(), actualJoke);
 
-        chuckNorrisDao.getJoke("test");
+    }
 
-        System.out.println(mockWebServer.takeRequest());
-
-
+    @AfterAll
+    static void tearDown() throws IOException {
+        mockWebServer.shutdown();
     }
 }
